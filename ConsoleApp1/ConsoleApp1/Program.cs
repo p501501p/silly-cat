95ed29bab79ce87f23fcb626f9f66be966305ad3
Kittisak Kankaeng <mm501501mm81@gmail.com>
2024-11-26 11:12:59 +0700

    add New Project

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+string name = "kittisak Khankaeng";
+string id = "673450031-4";
+Console.WriteLine("Hello, World!");
+Console.WriteLine("My Name is "+name);
+Console.WriteLine("My Student-Id is "+id);
